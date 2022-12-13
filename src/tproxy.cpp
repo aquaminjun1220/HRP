1e8c018f54f71104167e0d15b400ce7f322fb923
aquaminjun1220 <aquaminjun1220@gmail.com>
2022-12-14 06:45:47 +0900

    yey

--- src/tproxy.cpp
+++ src/tproxy.cpp
@@ -21,9 +21,7 @@
 #include "tproxy.hpp"
 
 typedef void handler_t(int);
-static std::map<uint16_t, uint16_t> omapSIP;
-static std::map<uint16_t, uint16_t> omapRTP;
-static std::map<std::string, int8_t> valid;
+static sockaddr_in sai;
 
 
 /**
@@ -93,50 +91,21 @@ static int open_tun(char *dev)
   } 
   close(sfd);
   std::cout << "INFO: setted tun" << std::endl;
-
   return fd;
 }
 
 static void init_rules(char *dev)
 {
-  system("ip rule add fwmark 1 lookup 100");
   char buffer[64];
-  sprintf(buffer, "ip route add 0.0.0.0/0 dev %s table 100", dev);
+  system("ip netns add vsip");
+  system("ip -n vsip link set lo up");
+  sprintf(buffer, "ip link set dev %s netns vsip", dev);
   system(buffer);
-  std::cout << "INFO: routing table initialized" << std::endl;
-
-  system("nft add table ip HRP");
-  system("nft add set HRP inc_portSIP \" { type inet_service ; } \"");
-  system("nft add set HRP inc_portRTP \" { type inet_service ; } \"");
-  system("nft add chain HRP output \" { type route hook output priority 0 ; } \"");
-  system("nft add chain HRP postroutingSIP \" { type filter hook postrouting priority -300 ; } \"");
-  system("nft add chain HRP postroutingRTP \" { type filter hook postrouting priority -301 ; } \"");
-  system("nft add rule HRP output udp sport @inc_portSIP meta mark set 1");
-  system("nft add rule HRP output udp sport @inc_portRTP meta mark set 1");
-  std::cout << "INFO: nftable initialized" << std::endl;
-}
-
-
-static int occupy_port(int oport)
-{
-  int osock;
-  struct sockaddr_in osock_addr;
-  if ((osock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-  {
-      perror("Failed to create osock");
-      exit(-1);
-  }
-
-  memset(&osock_addr, 0, sizeof(osock_addr));
-  osock_addr.sin_family = AF_INET;
-  osock_addr.sin_addr.s_addr = INADDR_ANY;
-  osock_addr.sin_port = htons(oport);
-  while (bind(osock, (const sockaddr *)(&osock_addr), sizeof(osock_addr)) < 0)
-  {
-      oport++;
-      osock_addr.sin_port = htons(oport);
-  }
-  return oport;
+  sprintf(buffer, "ip -n vsip link set %s up", dev);
+  system(buffer);
+  sprintf(buffer, "ip -n vsip route add 0.0.0.0/0 dev %s table main", dev);
+  system(buffer);
+  std::cout << "INFO: network namespace initialized" << std::endl;
 }
 
 static int rawsock_set()
@@ -149,52 +118,6 @@ static int rawsock_set()
   }
 
   return rsock;
-}
-
-static void nft_includeSIP(int sport)
-{
-  if (omapSIP.contains(sport))
-      return;
-  char buffer[96];
-  sprintf(buffer, "nft add element HRP inc_portSIP { %d }", sport);
-  system(buffer);
-  std::cout << "INFO: port " << sport << " is now routed to proxy" << std::endl;
-  int oport;
-  oport = 10000 + sport;
-  oport = occupy_port(oport);
-  omapSIP[sport] = oport;
-
-  sprintf(buffer, "nft add rule HRP postroutingSIP udp sport %d udp sport set %d notrack", oport, sport);
-  system(buffer);
-  std::cout << "INFO: oport " << oport << " is now statelessly snat to " << sport << std::endl;
-}
-
-static void nft_includeRTP(int sport)
-{
-  if (omapRTP.contains(sport))
-      return;
-  char buffer[96];
-  sprintf(buffer, "nft add element HRP inc_portRTP { %d }", sport);
-  system(buffer);
-  std::cout << "INFO: port " << sport << " is now routed to proxy" << std::endl;
-  int oport;
-  oport = 10000 + sport;
-  oport = occupy_port(oport);
-  omapRTP[sport] = oport;
-
-  sprintf(buffer, "nft add rule HRP postroutingRTP udp sport %d udp sport set %d notrack", oport, sport);
-  system(buffer);
-  std::cout << "INFO: oport " << oport << " is now statelessly snat to " << sport << std::endl;
-}
-
-static void nft_flushRTP()
-{
-  omapRTP.clear();
-  system("nft flush set HRP inc_portRTP");
-  std::cout << "INFO: all RTP ports are no longer routed to proxy" << std::endl;
-
-  system("nft flush chain HRP postroutingRTP");
-  std::cout << "INFO: all RTP ports are no longer SNAT" << std::endl;
 }
 
 /**************************************************************************
@@ -214,20 +137,15 @@ static int cread(int fd, uint8_t *buf, int n){
 
 void sigINThandler(int sig)
 {
-    int olderrno = errno;
-    sigset_t mask_all, prev_mask;
-    pid_t pid;
-    sigfillset(&mask_all);
-    sigprocmask(SIG_BLOCK, &mask_all, &prev_mask);
-    system("ip rule del fwmark 1 lookup 100");
-    system("ip route flush table 100");
-    std::cout << "INFO: routing table cleaned" << std::endl;
-
-    system("nft delete table HRP");
-    std::cout << "INFO: nftable cleaned" << std::endl;
-    sigprocmask(SIG_SETMASK, &prev_mask, NULL);
-    errno = olderrno;
-    exit(0);
+  int olderrno = errno;
+  sigset_t mask_all, prev_mask;
+  sigfillset(&mask_all);
+  sigprocmask(SIG_BLOCK, &mask_all, &prev_mask);
+  system("ip netns delete vsip");
+  std::cout << "INFO: network namespace cleaned" << std::endl;
+  sigprocmask(SIG_SETMASK, &prev_mask, NULL);
+  errno = olderrno;
+  exit(0);
 }
 
 /*
@@ -246,29 +164,6 @@ handler_t *Signal(int signum, handler_t *handler)
     return (old_action.sa_handler);
 }
 
-static std::string find_method(std::string& payload)
-{
-  std::string method;
-  int wordend = payload.find(' ');
-  method = payload.substr(0, wordend);
-  transform(method.begin(), method.end(), method.begin(), ::toupper);
-  if (valid.contains(method))
-    return method;
-  if (method != "SIP/2.0")
-    return "INVALID";
-  int lineend = payload.find('\r');
-  std::string response = payload.substr(wordend+1, lineend-(wordend+1));
-  transform(response.begin(), response.end(), response.begin(), ::toupper);
-  int C = payload.find("CSeq:");
-  C = payload.find(' ', C);
-  C = payload.find(' ', C+1);
-  C = C+1;
-  lineend = payload.find('\r', C);
-  method = payload.substr(C, lineend-C);
-  transform(method.begin(), method.end(), method.begin(), ::toupper);
-  return response + " (" + method + ")";
-}
-
 static int process_bmsg(uint8_t *in_buffer, uint8_t *out_buffer, int in_len)
 {
   uint8_t *iptr = in_buffer;
@@ -282,7 +177,6 @@ static int process_bmsg(uint8_t *in_buffer, uint8_t *out_buffer, int in_len)
   {
     struct iphdr ip;
     memcpy(&ip, iptr, sizeof(struct iphdr));
-    ip.check = 0;
     ip_len = &(reinterpret_cast<struct iphdr *>(iptr)->tot_len);
     iptr += sizeof(struct iphdr);
     in_len -= sizeof(struct iphdr);
@@ -294,6 +188,9 @@ static int process_bmsg(uint8_t *in_buffer, uint8_t *out_buffer, int in_len)
     std::cout << "Source Address: " << addr << std::endl;
     inet_ntop(AF_INET, &(ip.daddr), addr, sizeof(addr));
     std::cout << "Destination Address: " << addr << std::endl;
+    sai.sin_family = AF_INET;
+    sai.sin_addr.s_addr = ip.daddr;
+    sai.sin_port = 0;
     if (ip.protocol != IPPROTO_UDP)
     {
       std::cout << "Discarded Invalid Packet" << std::endl;
@@ -317,11 +214,8 @@ static int process_bmsg(uint8_t *in_buffer, uint8_t *out_buffer, int in_len)
     std::cout << "Source Address: " << addr << std::endl;
     inet_ntop(AF_INET6, &(ip6.ip6_dst), addr, sizeof(addr));
     std::cout << "Destination Address: " << addr << std::endl;
-    if (ip6.ip6_nxt != IPPROTO_UDP)
-    {
-      std::cout << "Discarded Invalid Packet" << std::endl;
-      return 0;
-    }
+    std::cout << "Igonring IPV6 Packet" << std::endl;
+    return 0;
   }
 
   // transport protocol is UDP
@@ -330,52 +224,29 @@ static int process_bmsg(uint8_t *in_buffer, uint8_t *out_buffer, int in_len)
   udp_len = &(reinterpret_cast<struct udphdr *>(iptr)->len);
   iptr += sizeof(struct udphdr);
   in_len -= sizeof(struct udphdr);
+  udp.check = 0;
+  memcpy(optr, &udp, sizeof(struct udphdr));
+  optr += sizeof(struct udphdr);
   std::cout << "UDP Packet Header:" << std::endl;
   std::cout << "Source Port: " << ntohs(udp.source) << std::endl;
   std::cout << "Destination Port: " << ntohs(udp.dest) << std::endl;
 
   // application is SIP, copy udp header and payload to out_buffer, modify source port.
   // do some SIP specific stuff
-  if (omapSIP.contains(ntohs(udp.source)))
+  if (ntohs(udp.source) == 5060)
   {
     std::cout << "SIP packet recieved:" << std::endl;
-    udp.source = htons(omapSIP[ntohs(udp.source)]);
-    memcpy(optr, &udp, sizeof(struct udphdr));
-    optr += sizeof(struct udphdr);
     memcpy(optr, iptr, in_len);
     optr += in_len;
-
-    std::string payload ((char *)iptr, in_len);
-    std::string method = find_method(payload);
-    std::cout << "SIP Method: " << method << std::endl;
-
-    if ((method == "INVITE") || (method == "200 OK (INVITE)"))
-    {
-      int sdp = payload.find("\r\n\r\n");
-      int idx = payload.find("m=audio", sdp);
-      int idk = payload.find(' ', idx+8);
-      uint16_t rtpport = std::stoi(payload.substr(idx+8, idk-(idx+8)));
-      nft_includeRTP(rtpport);
-      return optr - out_buffer;
-    }
-    else if ((method == "BYE") || (method == "200 OK (BYE)"))
-    {
-      nft_flushRTP();
-      return optr - out_buffer;
-    }
-    else if (method == "INVALID")
-    {
-      //return 0;
-      return optr - out_buffer;
-    }
     return optr - out_buffer;
   }
-
-  else if (omapRTP.contains(ntohs(udp.source)))
+  else
   {
-    return 0;
+    std::cout << "RTP packet recieved:" << std::endl;
+    memcpy(optr, iptr, in_len);
+    optr += in_len;
+    return optr - out_buffer;
   }
-  return 0;
 }
 
 int main()
@@ -386,36 +257,29 @@ int main()
   int in_len;
   uint8_t out_buffer[2000];
   int out_len;
-  sockaddr_in sai;
+  pid_t pid;
   sai.sin_family = AF_INET;
-  sai.sin_addr.s_addr = INADDR_ANY;
+  sai.sin_addr.s_addr = 0;
   sai.sin_port = 0;
 
   std::cout << "Press any key to start" << std::endl;
   std::cin.get();
 
-  valid["REGISTER"] = 1;
-  valid["INVITE"] = 1;
-  valid["ACK"] = 1;
-  valid["BYE"] = 1;
-  valid["CANCEL"] = 1;
-  valid["UPDATE"] = 1;
-  valid["REFER"] = 1;
-  valid["PRACK"] = 1;
-  valid["SUBSCRIBE"] = 1;
-  valid["NOTIFY"] = 1;
-  valid["PUBLISH"] = 1;
-  valid["MESSAGE"] = 1;
-  valid["INFO"] = 1;
-  valid["OPTIONS"] = 1;
+  //if ((pid = fork()) < 0)
+  //{
+  //  perror("Failed to fork");
+  //  exit(-1);
+  //}
+  //else if (pid == 0)
+  //{
+
+  //}
 
   tun = open_tun(dev);
   rsock = rawsock_set();
 
   init_rules(dev);
   Signal(SIGINT, sigINThandler);
-
-  nft_includeSIP(5060);
 
   while (1)
   {
